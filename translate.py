f2b923722c886104edd59fd0e1d97604d9471f5b
MasterBabol <babol@live.co.kr>
2019-08-05 15:04:19 +0200

    Add substruct member variable read support

--- translate.py
+++ translate.py
@@ -6,6 +6,7 @@ import sys
 import re
 import io
 import copy
+import os
 
 class CursoredData:
     def __init__(self, bytesio_data):
@@ -49,23 +50,25 @@ def transform_log_to_readable(log_format, log):
     log['tier'] = log_format['tiers'][log['tier']]
     log['status'] = log_format['valid'][log['status']]
     for k in log['payload']:
-        if k == 'subject':
+        if 'subject' in k:
             log['payload'][k] = log_format['psubject'][log['payload'][k]]
-        elif k == 'action':
+        elif 'action' in k:
             log['payload'][k] = log_format['paction'][log['payload'][k]]
-        elif k == 'fromObject':
+        elif 'fromObject' in k:
             log['payload'][k] = log_format['pobject'][log['payload'][k]]
-        elif k == 'toObject':
+        elif 'toObject' in k:
             log['payload'][k] = log_format['pobject'][log['payload'][k]]
         
     return log
 
-def print_dict(log, level=0):
+def print_dict(log, level=0, additional_line_per_item=False):
     for k, v in log.items():
         if isinstance(v, dict):
             print_dict(v, level+1)
         else:
             print(' ' * level * 2 + k + ': ' + str(v))
+            if additional_line_per_item is True:
+                print()
 
 def print_log(log):
     print_dict(log)
@@ -121,32 +124,71 @@ def parse_enums_to_list(format_rawcontent, target_name):
     
     return ret_list
 
-def parse_struct_to_list_of_tuple(format_rawcontent, target_name):
+def parse_struct_to_list_of_tuple(format_rawcontent, target_name, substruct_types):
+    _, ret_list = parse_struct_to_name_and_list_of_tuple(format_rawcontent, target_name, substruct_types)
+    return ret_list
+
+def parse_struct_to_name_and_list_of_tuple(format_rawcontent, target_prefix, substruct_types):
     ret_list = []
-    re_mat_pat = '\s*typedef\s+struct\s+_' + target_name + '\s*{((?:\s*[^}]+\s*,?)+)}\s*.*\s*;'
-    re_mem_pat = '\s*((?:uint\d+_t)+)\s+(\w+)\s*(?:\[(\d+)\])?\s*;\s*'
+    re_mat_pat = '\s*typedef\s+struct\s+_' + target_prefix + '(w*)\s*{((?:\s*[^}]+\s*,?)+)}\s*.*\s*;'
+    re_mem_pat = '\s*((?:[^;])+)\s+(\w+)\s*(?:\[(\d+)\])?\s*;\s*'
     
     m = re.search(re_mat_pat, format_rawcontent)
-    raw_members_str = m.group(1)
+    target_name = m.group(1)
+    raw_members_str = m.group(2)
     raw_members = raw_members_str.splitlines()
     
     for rmem in raw_members:
         m = re.search(re_mem_pat, rmem)
         if m is not None:
-            new_mem = (m.group(1), m.group(2), m.group(3))
-            ret_list.append(new_mem)
+            tname = m.group(1)
+            vname = m.group(2)
+            if tname in substruct_types:
+                tlist = substruct_types[tname]
+                its = int(m.group(3)) if m.group(3) is not None else 1
+                for i in range(0, its):
+                    itname = ('[' + str(i) + ']') if its > 1 else ''
+                    for sstype in tlist:
+                        new_mem = (sstype[0], vname + itname + '.' + sstype[1], sstype[2])
+                        ret_list.append(new_mem)
+            else:
+                new_mem = (m.group(1), m.group(2), m.group(3)) # type_name, var_name, counts
+                ret_list.append(new_mem)
     
-    return ret_list
+    return target_name, ret_list
 
-def parse_payload_types(format_rawcontent, log_types):
+def parse_payload_types_to_list(format_rawcontent, log_types, substruct_types):
     payload_types = []
     
     for ltype in log_types:
-        new_payload_type = parse_struct_to_list_of_tuple(format_rawcontent, "PAYLOAD_FOR_LOG_TYPE_" + ltype)
+        new_payload_type = parse_struct_to_list_of_tuple(format_rawcontent, "PAYLOAD_FOR_LOG_TYPE_" + ltype, substruct_types)
         payload_types.append(new_payload_type)
     
     return payload_types
+
+def parse_substruct_types_to_dict(format_rawcontent):
+    substruct_types = {}
     
+    re_mat_pat = '\s*typedef\s+struct\s+_STRUCT_(\w*)\s*{((?:\s*[^}]+\s*,?)+)}\s*.*\s*;'
+    re_mat_pat_c = re.compile(re_mat_pat)
+    re_mem_pat = '\s*((?:uint\d+_t)+)\s+(\w+)\s*(?:\[(\d+)\])?\s*;\s*'
+    
+    matches = re_mat_pat_c.finditer(format_rawcontent)
+    for m in matches:
+        tname = m.group(1)
+        tstr = m.group(2)
+        tmembers = tstr.splitlines()
+        tlist = []
+        
+        for rmem in tmembers:
+            m = re.search(re_mem_pat, rmem)
+            if m is not None:
+                new_mem = (m.group(1), m.group(2), m.group(3))
+                tlist.append(new_mem)
+        
+        substruct_types[tname] = tlist
+    
+    return substruct_types
 
 def translate(log_filename, format_filename):
     with open(format_filename) as f:
@@ -161,7 +203,8 @@ def translate(log_filename, format_filename):
     log_paction = parse_enums_to_list(format_rawcontent, "PAYLOAD_ACTION")
     log_pobject = parse_enums_to_list(format_rawcontent, "PAYLOAD_OBJECT")
     
-    payload_types = parse_payload_types(format_rawcontent, log_types)
+    substruct_types = parse_substruct_types_to_dict(format_rawcontent)
+    payload_types = parse_payload_types_to_list(format_rawcontent, log_types, substruct_types)
     
     log_format = {}
     log_format['types'] = log_types
@@ -171,6 +214,7 @@ def translate(log_filename, format_filename):
     log_format['paction'] = log_paction
     log_format['pobject'] = log_pobject
     log_format['ptypes'] = payload_types
+    log_format['sstypes'] = substruct_types
     
     translated_logs = []
     
@@ -195,13 +239,18 @@ if __name__ == "__main__":
         log_filename = sys.argv[1] if len(sys.argv) > 1  else "log.txt"
         format_filename = sys.argv[2] if len(sys.argv) > 2 else "log_format.h"
     
+        print('[!] Processing...')
         formats, logs = translate(log_filename, format_filename)
+        
+        logdir = './result/' + log_filename + '/'
+        if not os.path.exists(logdir):
+            os.makedirs(logdir)
         
         files = {}
         for ltype in formats['types']:
-            files[ltype] = open('log_of_' + ltype + '.txt', 'w')
+            files[ltype] = open(logdir + ltype + '.txt', 'w')
         
         for l in logs:
-            print_log(l)
             save_log(files[l['type']], l)
+        print('[+] Finished.')
         
